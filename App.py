a7051ba243303971b5934c389a7b349822534632
cs <kchunshen.@gmail.com>
2025-01-18 16:27:49 +0800

    init

---
+++ App.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from PIL import Image
+
+
+st.title('AMK HG CSN')
+image = Image.open('Data\\Team.jpg')
+# Display the image in the Streamlit app
+st.image(image, caption='Members', use_container_width =True)
---
+++ Doc/start.bat
@@ -0,0 +1,2 @@
+cd C:\Users\kchun\OneDrive\fengshan\Stat-tracking
+D:\environments\streamlit\Scripts\python -m streamlit run App.py
