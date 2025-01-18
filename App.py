909dab73d97f73c8e21343fb3ed15bd7f6e668c9
cs <kchunshen.@gmail.com>
2025-01-18 18:46:10 +0800

    add page navigation

--- App.py
+++ App.py
@@ -8,3 +8,7 @@ image = Image.open('Data/Team.jpg')
 
 # Display the image in the Streamlit app
 st.image(image, caption='Members', use_container_width =True)
+
+if st.button("Go to Stats page"):
+    st.switch_page("pages/1-Stats Tracking.py")
+
