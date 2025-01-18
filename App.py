b0fe30aa408519339a2f5b8cc4e98167f2c11194
cs <kchunshen.@gmail.com>
2025-01-18 18:35:58 +0800

    image fix

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ import os
 st.title('AMK HG CSN')
 st.write(os.getcwd())
 st.write(os.listdir())
-image = Image.open('..\\Data\\Team.jpg')
+image = Image.open('Data/Team.jpg')
 
 # Display the image in the Streamlit app
 st.image(image, caption='Members', use_container_width =True)
